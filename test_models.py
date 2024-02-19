4a97b9d0fc3cae9799cb11697eba30cb8c2e4191
disappearcompletely <d.komarov2019@gmail.com>
2024-02-20 01:56:13 +0300

    keycloak

--- test_models.py
+++ test_models.py
@@ -5,31 +5,25 @@ from models import MenuItem
 class ModelsTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        # Устанавливаем конфигурацию тестовой базы данных один раз для всего класса
         app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dmitriy:321123@localhost/club_test'
 
     def setUp(self):
-        # Создаем таблицы в контексте приложения
         with app.app_context():
             db.create_all()
 
     def tearDown(self):
-        # Удаляем таблицы и очищаем сессию в контексте приложения
         with app.app_context():
             db.session.remove()
             db.drop_all()
 
     def test_menu_item_model(self):
-        # Работаем с базой данных в контексте приложения
         with app.app_context():
             # Очищаем таблицу MenuItem перед тестом
             db.session.query(MenuItem).delete()
             db.session.commit()
 
-            # Добавляем новый элемент в базу данных
             item = MenuItem(name="Test Pizza", price=100.0)
             db.session.add(item)
             db.session.commit()
 
-            # Проверяем, что в таблице ровно один элемент
             self.assertEqual(MenuItem.query.count(), 1)
